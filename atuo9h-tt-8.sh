eeefde4211a0978da69168e9ad78818f4a419a00
lastgikw203 <55953948+lastgikw203@users.noreply.github.com>
2019-11-03 11:11:24 +0800

    Update atuo9h-tt-8.sh

--- atuo9h-tt-8.sh
+++ atuo9h-tt-8.sh
@@ -5,7 +5,7 @@ mv -f /root/last203/cron /etc/pam.d/cron &&
 rm -rf /root/last203/ &&
 git clone https://github.com/MauroS5/9Hits-AutoInstall.git &&
 chmod -R 777 9Hits-AutoInstall &&
-9Hits-AutoInstall/install.sh "2" "64730c0a01d3e9cec964b0b03b5c142a" "6" "15" &&
+9Hits-AutoInstall/install.sh "2" "7081ae5435ea52a14c421c9a0a53e136" "7" "15" &&
 rm install.sh &&
 cron &&
 ps -ef
