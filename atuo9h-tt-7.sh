ae32e18bb098f93d31f223fdd73c215761522b5f
lastgikw203 <55953948+lastgikw203@users.noreply.github.com>
2019-11-06 20:50:10 +0800

    Update atuo9h-tt-7.sh

--- atuo9h-tt-7.sh
+++ atuo9h-tt-7.sh
@@ -1,11 +1,11 @@
+
 apt-get -y upgrade &&
-apt install -y whiptail cron &&
-git clone https://github.com/lastgikw203/last203.git &&
-mv -f /root/last203/cron /etc/pam.d/cron &&
-rm -rf /root/last203/ &&
+apt install -y whiptail cron bzip2 &&
+cd /etc/pam.d/ &&
+sed -i "s/required/sufficient/g" cron &&
+cd ~ &&
+/etc/init.d/cron restart &&
 git clone https://github.com/MauroS5/9Hits-AutoInstall.git &&
 chmod -R 777 9Hits-AutoInstall &&
 9Hits-AutoInstall/install.sh "2" "7081ae5435ea52a14c421c9a0a53e136" "7" "15" &&
-rm install.sh &&
-cron &&
-ps -ef
+rm install.sh
